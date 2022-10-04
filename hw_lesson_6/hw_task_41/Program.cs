d3c684882c1b8cf787343c5715b0bab7e5977fea
Krivoukhov Igor <igorkrivoukhov@gmail.com>
2022-10-04 16:53:46 +0300

    hw_lesson_6 Done

---
+++ hw_lesson_6/hw_task_41/Program.cs
@@ -0,0 +1,25 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. Ввод чисел останавливается при помощи ввода стоп-слова "stop"
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+string Read (string message)
+{
+Console.WriteLine(message);
+return Console.ReadLine();
+}
+string mess = Read ("Введите любое число , для остановки введите stop");
+int count = 0;
+while ( mess != "stop")
+{
+int i = Convert.ToInt32(mess);
+if ( i > 0)
+{
+   count = count + 1;
+   mess = Read("Введите любое число , для остановки введите stop");
+}
+else
+{
+   mess = Read("Введите любое число , для остановки введите stop");
+}
+}
+Console.WriteLine($"Вы ввели {count} чисел больше 0");
---
+++ hw_lesson_6/hw_task_43/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+//y-y = k1*x +b1-(K2*x+b2) / 0 = k1*x +b1 - k2*x - b2
+
+double Read (string message)
+{
+Console.WriteLine(message);
+return (Convert.ToDouble(Console.ReadLine()));
+}
+double b1 = Read("Введите координату b1");
+double k1 = Read("Введите координату k1");
+double b2 = Read("Введите координату b2");
+double k2 = Read("Введите координату k2");
+double x = (b1-b2)/(k2-k1);
+double y = k2 * x + b2;
+Console.WriteLine($"Координата точки пересечения двух прямых х:{x} ; y:{y}");
